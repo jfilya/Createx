94b387bb21b98d669c1d3cf3b735c13640f5e2eb
jfilya <95137071+jfilya@users.noreply.github.com>
2022-01-18 21:56:58 +0300

    createx

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,5 @@
 const btnOpen = document.getElementById('btn-open-sing-in');
+const btnOpen1 = document.getElementById('btn-open-sing-in1');
 const modal = document.getElementById('wrapper-modal-sing-in');
 
 const overlay = document.getElementById('overlay');
@@ -7,7 +8,9 @@ const overlay = document.getElementById('overlay');
 btnOpen.addEventListener('click', () => {
     modal.classList.add('active');
 });
-
+btnOpen1.addEventListener('click', () => {
+    modal.classList.add('active');
+});
 const closeModel = () => {
     modal.classList.remove('active');
 };
@@ -17,6 +20,7 @@ overlay.addEventListener('click', closeModel);
 
 
 const btnOpenUp = document.getElementById('btn-open-sing-up');
+const btnOpenUp1 = document.getElementById('btn-open-sing-up1');
 const modalUp = document.getElementById('wrapper-modal-sing-up');
 
 const overlayUp = document.getElementById('overlayup');
@@ -25,7 +29,9 @@ const overlayUp = document.getElementById('overlayup');
 btnOpenUp.addEventListener('click', () => {
     modalUp.classList.add('active');
 });
-
+btnOpenUp1.addEventListener('click', () => {
+    modalUp.classList.add('active');
+});
 const closeModelUp = () => {
     modalUp.classList.remove('active');
 };
