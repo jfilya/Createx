6406c7c4068187035d2540bf21b40dd01a89a040
jfilya <95137071+jfilya@users.noreply.github.com>
2022-02-03 19:42:59 +0300

    createx

---
+++ js/slider-event.js
@@ -0,0 +1,42 @@
+let position = 0;
+const slidesToShow = 3;
+const slidesToScroll = 1;
+const container = document.querySelector('.slider');
+const track = document.querySelector('.line');
+const btnPrev =  document.querySelector('.arrow-left'); 
+const btnNext = document.querySelector('.arrow-right'); 
+const items = document.querySelectorAll('.event');
+const itemsCount = items.length;
+const itemWidth = container.clientWidth / slidesToShow ;
+const movePosition = slidesToScroll * itemWidth;
+
+items.forEach((item) => {
+    item.style.minWidth = `${itemWidth}px`;
+});
+
+btnNext.addEventListener('click',() =>{
+const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+setPosition();
+checkBtns();
+});
+
+
+btnPrev.addEventListener('click',() =>{
+const itemsLeft = Math.abs(position) / itemWidth;
+position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+setPosition();
+checkBtns();
+});
+
+const setPosition= () =>{
+track.style.transform = `translateX(${position}px)`;
+}
+
+const checkBtns = () => {
+btnPrev.disabled = position === 0;
+btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+};
+
+checkBtns();
+
